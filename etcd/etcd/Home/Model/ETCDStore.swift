33331bf657aa5a48b071256b6c27d8a41d8a5a32
FaceBook <FaceBook@inke.cn>
2022-03-27 21:50:15 +0800

    修改数据显示

--- etcd/etcd/Home/Model/ETCDStore.swift
+++ etcd/etcd/Home/Model/ETCDStore.swift
@@ -69,13 +69,16 @@ extension HomeViewModel {
         do {
             let data = try? c.all()
             let pairs = try JSONDecoder().decode([PairStore].self, from: data!)
+            var items = [ETCDItem]()
             for key in pairs {
                 let dir = key.key.components(separatedBy: "/")
                 let etcdRoot = ETCDItem.init(value: dir.first!)
                 if dir.count > 1{
                     etcdRoot.add(child: coverItem(etcdRoot: etcdRoot, dir: dir,count: 1))
                 }
+                items.append(etcdRoot)
             }
+            print("items------------\(items)")
             return []
         } catch  {
             print(error)
