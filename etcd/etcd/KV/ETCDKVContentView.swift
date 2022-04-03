d317f713baa516d05d2f7dd51d2e02a1c86862c2
taoshumin_vendor <taoshumin@foxmail.com>
2022-04-04 00:31:13 +0800

    feat: 添加kv详情页

--- etcd/etcd/KV/ETCDKVContentView.swift
+++ etcd/etcd/KV/ETCDKVContentView.swift
@@ -8,12 +8,64 @@
 
 import SwiftUI
 
+/*
+ Circle()
+     .fill(.red)
+     .frame(width: 44, height: 44, alignment: .center)
+     .overlay(Text("Hello").foregroundColor(.white))
+ */
+
+
+extension View {
+    func withDefaultHeaderLabelView(title: String,fontSize: CGFloat = 13,color: Color =  Color(hex: "#375B7E")) -> some View {
+        Label {
+            Text(title)
+                .withDefaultContentTitle(fontColor: color,fontSize: fontSize)
+        } icon: {
+           Image(systemName: "books.vertical.circle")
+                .withDefaultImage(foreColor: color, width: 22.0)
+                .padding(.leading,4.0)
+                .padding(.top,8.0)
+                .padding(.bottom,8.0)
+                .padding(.trailing,4.0)
+        }
+    }
+    
+    func withDefaultOnlyImageButton(name: String, color: Color = Color(hex: "#375B7E") ,size: CGFloat = 13) -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: name)
+                 .withDefaultImage(foreColor: color, width: size)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
 struct ETCDKVContentView: View {
     @EnvironmentObject var storeObj : ItemStore
+    var address : String = "0.0.0.0:2379"
     var body: some View {
-        List{
-            ForEach(storeObj.all(),id: \.key) { item in
-                Text(item.key)
+        ZStack {
+            List{
+                Section {
+                    ForEach(storeObj.all(),id: \.key) { item in
+                        HStack {
+                            Text(item.key)
+                                .withDefaultSubContentTitle(fontColor: .black, fontSize: 12.0)
+                                .padding(10.0)
+                        }
+                    }
+                } header: {
+                    HStack {
+                        withDefaultHeaderLabelView(title: address)
+                        Spacer()
+                        withDefaultOnlyImageButton(name: "arrow.clockwise.circle",color: .orange,size: 20.0)
+                        withDefaultOnlyImageButton(name: "folder.badge.minus",color: .red,size: 22.0)
+                        withDefaultOnlyImageButton(name: "plus.circle",color: .green,size: 18.0)
+                        withDefaultOnlyImageButton(name: "magnifyingglass",size: 18.0)
+                    }
+                }
             }
         }
     }
