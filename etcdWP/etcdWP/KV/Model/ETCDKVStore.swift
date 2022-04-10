c722603f5a9ef282e4d28aefbcf9718bff54017f
FaceBook <FaceBook@inke.cn>
2022-04-10 12:14:33 +0800

    [5.1.20][MOD]:更改解析的问题

--- etcdWP/etcdWP/KV/Model/ETCDKVStore.swift
+++ etcdWP/etcdWP/KV/Model/ETCDKVStore.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
-struct PairStore: Codable {
+import ObjectMapper
+struct PairStore:Codable,Mappable {
+
     var key: String
     var value: String
-    var create_revision: Int64
-    var mod_revision: Int64
-    var version: Int64
+    var create_revision: Int64?
+    var mod_revision: Int64?
+    var version: Int64?
     var pairs: [PairStore]?
+    
+     init?(map: Map){
+         self.key = try! map.value("key")
+         self.value = try! map.value("value")
+         self.create_revision = try? map.value("create_revision")
+         self.mod_revision = try? map.value("mod_revision")
+         self.version = try? map.value("version")
+         self.pairs = try? map.value("pairs")
+    }
+    mutating func mapping(map: Map) {
+        key                 <- map["key"]
+        value               <- map["value"]
+        create_revision <- map ["create_revision"]
+        mod_revision           <- map["mod_revision"]
+        version           <- map["version"]
+        pairs            <- map["pairs"]
+    }
+    
+
 }
 
 extension PairStore {
