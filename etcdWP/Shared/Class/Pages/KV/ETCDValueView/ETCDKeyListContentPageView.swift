59f6616be1ba24d706d8072bcdb9966634517cb5
FaceBook <FaceBook@inke.cn>
2022-05-20 21:51:52 +0800

    [MOD]:优化结构梳理状态的问题

---
+++ etcdWP/Shared/Class/Pages/KV/ETCDValueView/ETCDKeyListContentPageView.swift
@@ -0,0 +1,41 @@
+//
+//  ETCDKeyListContentPageView.swift
+//  etcdWP
+//
+//  Created by FaceBook on 2022/5/20.
+//
+
+import SwiftUI
+
+struct ETCDKeyListContentPageView: View {
+    @StateObject var storeObj: ItemStore
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("当前页:  \(storeObj.realeadData.GetCurrentPage())  ")
+                .foregroundColor(.secondary)
+                .font(.caption)
+            Spacer()
+            Button {
+                storeObj.Last()
+            } label: {
+                Text("上一页")
+                    .font(.caption)
+                    .foregroundColor(.white)
+            }
+            Spacer()
+            Button {
+                storeObj.Next()
+            } label: {
+                Text("下一页")
+                    .font(.caption)
+                    .foregroundColor(.white)
+            }
+            Spacer()
+            Text("总数:  \(storeObj.realeadData.GetKvCount())  ")
+                .foregroundColor(.secondary)
+                .font(.caption)
+            Spacer()
+        }
+    }
+}
