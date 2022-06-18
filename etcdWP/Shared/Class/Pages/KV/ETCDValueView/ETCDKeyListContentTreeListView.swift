393994430a98e7e627dde0e4c5683962a2c33e4a
FaceBook <FaceBook@inke.cn>
2022-06-18 13:08:38 +0800

    [MOD]:格式化代码

--- etcdWP/Shared/Class/Pages/KV/ETCDValueView/ETCDKeyListContentTreeListView.swift
+++ etcdWP/Shared/Class/Pages/KV/ETCDValueView/ETCDKeyListContentTreeListView.swift
@@ -42,7 +42,7 @@ struct ETCDKeyListContentTreeListView: View {
             }else{
                 EmptyView()
             }
-
+            
             Spacer()
         }.background(Color(hex: "#221C27"))
             .onAppear{
@@ -50,135 +50,135 @@ struct ETCDKeyListContentTreeListView: View {
                     treeModel = try?  await storeObj.treeItem()
                 }
             }
-         .popover(isPresented: $isShowingUpdatePopover,arrowEdge: .trailing) {
-            switch isDefaultSelectType {
-            case 0:
-                VStack {
-                    Section(header: Text("更新键值").foregroundColor(.white).font(.system(size: 12))) {
-                        TextEditor(text: $textValue)
-                            .foregroundColor(Color.white)
-                            .font(.system(size: 12))
-                            .lineSpacing(1.5)
-                            .disableAutocorrection(true)
-                            .allowsTightening(true)
-                            .padding(.bottom,5)
-                            .padding(10)
-                            .background(Color.gray.opacity(0.15))
-                            .cornerRadius(10)
-                            .clipped()
-                    }
-                    .frame(width: 180, alignment: .center)
-                    .padding(.top,15)
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Button {
-                            isShowingUpdatePopover.toggle()
-                        } label: {
-                            Text("取消")
+            .popover(isPresented: $isShowingUpdatePopover,arrowEdge: .trailing) {
+                switch isDefaultSelectType {
+                case 0:
+                    VStack {
+                        Section(header: Text("更新键值").foregroundColor(.white).font(.system(size: 12))) {
+                            TextEditor(text: $textValue)
+                                .foregroundColor(Color.white)
                                 .font(.system(size: 12))
-                                .foregroundColor(.white)
+                                .lineSpacing(1.5)
+                                .disableAutocorrection(true)
+                                .allowsTightening(true)
+                                .padding(.bottom,5)
+                                .padding(10)
+                                .background(Color.gray.opacity(0.15))
+                                .cornerRadius(10)
+                                .clipped()
                         }
-                        .padding(.trailing,20)
+                        .frame(width: 180, alignment: .center)
+                        .padding(.top,15)
                         
-                        Button {
-                            defer {isShowingUpdatePopover.toggle()}
-                            
-                            do {
-                                guard !textValue.isEmpty else {
-                                    throw NSError.init(domain: "键值不能输入为空", code: 400)
-                                }
-                                let resp =  self.storeObj.Put(key: (self.storeObj.realeadData.currentKv?.sep_key)!, value: textValue)
-                                if resp?.status != 200 {
-                                    throw NSError.init(domain: resp?.message ?? "", code: resp?.status ?? 500)
-                                }
-                                self.storeObj.realeadData.currentKv?.value = textValue
-                                Reaload()
-                            } catch  {
-                                print(error.localizedDescription)
+                        Spacer()
+                        
+                        HStack {
+                            Button {
+                                isShowingUpdatePopover.toggle()
+                            } label: {
+                                Text("取消")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.white)
                             }
+                            .padding(.trailing,20)
                             
-                        } label: {
-                            Text("确定")
-                                .font(.system(size: 12))
-                                .foregroundColor(.white)
+                            Button {
+                                defer {isShowingUpdatePopover.toggle()}
+                                
+                                do {
+                                    guard !textValue.isEmpty else {
+                                        throw NSError.init(domain: "键值不能输入为空", code: 400)
+                                    }
+                                    let resp =  self.storeObj.Put(key: (self.storeObj.realeadData.currentKv?.sep_key)!, value: textValue)
+                                    if resp?.status != 200 {
+                                        throw NSError.init(domain: resp?.message ?? "", code: resp?.status ?? 500)
+                                    }
+                                    self.storeObj.realeadData.currentKv?.value = textValue
+                                    Reaload()
+                                } catch  {
+                                    print(error.localizedDescription)
+                                }
+                                
+                            } label: {
+                                Text("确定")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.white)
+                            }
                         }
-                    }
-                    .padding(.bottom,20)
-                    Spacer()
-                }
-                .frame(width: 280, height: 320)
-            case 1:
-                VStack {
-                    Spacer()
-                    Section(header: Text("查看键值详情").foregroundColor(.white).font(.system(size: 12))) {
-                        Text("CreateRevision： \(String(describing: storeObj.realeadData.currentKv?.create_revision ?? 0))")
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
-                        Text("ModRevision： \(String(describing: storeObj.realeadData.currentKv?.mod_revision ?? 0))")
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
-                        Text("Version： \(String(describing: storeObj.realeadData.currentKv?.version ?? 0))")
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
-                        Text("Lease： \(String(describing: storeObj.realeadData.currentKv?.lease ?? 0))")
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
+                        .padding(.bottom,20)
                         Spacer()
                     }
-                    .padding(.top,15)
-                    Spacer()
-                }
-                .frame(width: 180, height: 210)
-            default:
-                VStack {
-                    Section(header: Text("更新键值").foregroundColor(.white).font(.system(size: 12))) {
-                        TextEditor(text: $textValue)
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
+                    .frame(width: 280, height: 320)
+                case 1:
+                    VStack {
+                        Spacer()
+                        Section(header: Text("查看键值详情").foregroundColor(.white).font(.system(size: 12))) {
+                            Text("CreateRevision： \(String(describing: storeObj.realeadData.currentKv?.create_revision ?? 0))")
+                                .font(.system(size: 12))
+                                .foregroundColor(.white)
+                            Text("ModRevision： \(String(describing: storeObj.realeadData.currentKv?.mod_revision ?? 0))")
+                                .font(.system(size: 12))
+                                .foregroundColor(.white)
+                            Text("Version： \(String(describing: storeObj.realeadData.currentKv?.version ?? 0))")
+                                .font(.system(size: 12))
+                                .foregroundColor(.white)
+                            Text("Lease： \(String(describing: storeObj.realeadData.currentKv?.lease ?? 0))")
+                                .font(.system(size: 12))
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        .padding(.top,15)
+                        Spacer()
                     }
-                    .frame(width: 180, alignment: .center)
-                    .padding(.top,15)
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Button {
-                            isShowingUpdatePopover.toggle()
-                        } label: {
-                            Text("取消")
+                    .frame(width: 180, height: 210)
+                default:
+                    VStack {
+                        Section(header: Text("更新键值").foregroundColor(.white).font(.system(size: 12))) {
+                            TextEditor(text: $textValue)
                                 .font(.system(size: 12))
                                 .foregroundColor(.white)
                         }
-                        .padding(.trailing,20)
+                        .frame(width: 180, alignment: .center)
+                        .padding(.top,15)
                         
-                        Button {
-                            defer {isShowingUpdatePopover.toggle()}
-                            
-                            do {
-                                guard !textValue.isEmpty else {
-                                    throw NSError.init(domain: "键值不能输入为空", code: 400)
-                                }
-                                let resp =  self.storeObj.Put(key: (self.storeObj.realeadData.currentKv?.sep_key)!, value: textValue)
-                                if resp?.status != 200 {
-                                    throw NSError.init(domain: resp?.message ?? "", code: resp?.status ?? 500)
-                                }
-                                Reaload()
-                            } catch  {
-                                print(error.localizedDescription)
+                        Spacer()
+                        
+                        HStack {
+                            Button {
+                                isShowingUpdatePopover.toggle()
+                            } label: {
+                                Text("取消")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.white)
                             }
-                        } label: {
-                            Text("确定")
-                                .font(.system(size: 12))
-                                .foregroundColor(.white)
+                            .padding(.trailing,20)
+                            
+                            Button {
+                                defer {isShowingUpdatePopover.toggle()}
+                                
+                                do {
+                                    guard !textValue.isEmpty else {
+                                        throw NSError.init(domain: "键值不能输入为空", code: 400)
+                                    }
+                                    let resp =  self.storeObj.Put(key: (self.storeObj.realeadData.currentKv?.sep_key)!, value: textValue)
+                                    if resp?.status != 200 {
+                                        throw NSError.init(domain: resp?.message ?? "", code: resp?.status ?? 500)
+                                    }
+                                    Reaload()
+                                } catch  {
+                                    print(error.localizedDescription)
+                                }
+                            } label: {
+                                Text("确定")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.white)
+                            }
                         }
+                        Spacer()
                     }
-                    Spacer()
+                    .frame(width: 280, height: 320)
                 }
-                .frame(width: 280, height: 320)
             }
-        }
     }
 }
 
