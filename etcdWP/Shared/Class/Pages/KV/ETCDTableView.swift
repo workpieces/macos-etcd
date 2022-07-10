10fed00ba173d750c99b67860e9802df2a950197
FaceBook <FaceBook@inke.cn>
2022-07-10 15:38:14 +0800

    [MOD]:修改显示

--- etcdWP/Shared/Class/Pages/KV/ETCDTableView.swift
+++ etcdWP/Shared/Class/Pages/KV/ETCDTableView.swift
@@ -11,33 +11,6 @@ import SnapKit
 
 #if os(macOS)
 import Cocoa
-
-struct ETCDTableViewRepresentableBootcamp : NSViewRepresentable{
-    @EnvironmentObject var storeObj : ItemStore
-    
-    func makeNSView(context: Context) -> ETCDTableView {
-        let  etcdViewController  = ETCDTableView()
-        return etcdViewController;
-    }
-    
-    func updateNSView(_ nsView: ETCDTableView, context: Context) {
-        Task{
-            await  nsView.reloadData(storeObj)
-        }
-    }
-    
-    typealias NSViewType = ETCDTableView
-    
-    
-    func makeCoordinator() -> ETCDCoordinator {
-      return ETCDCoordinator()
-    }
-
-    class ETCDCoordinator: NSObject {
-        
-    }
-}
-    
 class ETCDTableView: NSView
 {
     fileprivate var scrollView : NSScrollView!
