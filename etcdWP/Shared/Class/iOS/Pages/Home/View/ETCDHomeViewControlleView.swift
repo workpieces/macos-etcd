13e51bb605c7fe5a2f0506229c975707c8750386
Google <zlbfzl@163.com>
2022-07-21 07:18:37 +0800

    [1.0.8][MOD]:修改显示

--- etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
+++ etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ETCDHomeViewControlleView: View {
+    
+    @EnvironmentObject var homeData: HomeViewModel
+    let closePublisher = NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)
+    let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(self.homeData.ectdClientList.indices,id: \.self) { index in
+            CardItemView(options:homeData.ectdClientList[index],idx: index)
+        } .onReceive(timer, perform: {_ in
+            Task {
+                await homeData.WatchListenEtcdClient()
+            }
+        }).onDisappear {
+            self.timer.upstream.connect().cancel()
+        }.onAppear{
+            Task {
+                await homeData.WatchListenEtcdClient()
+            }
+        }.onReceive(closePublisher) { _ in
+            self.homeData.ectdClientList.removeAll()
+        }
     }
 }
 
--- etcdWP/Shared/Class/iOS/Pages/View/draw/ETCDRootViewControllView.swift
+++ etcdWP/Shared/Class/iOS/Pages/View/draw/ETCDRootViewControllView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ETCDRootViewControllView: View {
-    
     init() {
         UITabBar.appearance().isHidden = true
     }
