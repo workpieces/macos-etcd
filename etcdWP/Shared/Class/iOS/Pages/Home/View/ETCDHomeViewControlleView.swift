56ddb469b3f614c9042d14811597c14907af068f
Google <zlbfzl@163.com>
2022-07-22 07:41:58 +0800

    [1.0.8][MOD]:

--- etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
+++ etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
@@ -14,40 +14,45 @@ struct ETCDHomeViewControlleView: View {
     let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Spacer()
+        NavigationView{
+            VStack(spacing: 0) {
                 HStack {
-                    Text(LocalizedStringKey("创建ETCD客户端"))
-                        .withDefaultContentTitle()
+                    Spacer()
+                    NavigationLink(
+                        destination:  Text(LocalizedStringKey("创建ETCD客户端"))
+                            .withDefaultContentTitle(),
+                        label: {
+                            HStack {
+                                Text(LocalizedStringKey("创建ETCD客户端"))
+                                    .withDefaultContentTitle()
+                            }
+                            .padding(DefaultSpacePadding)
+                            .buttonStyle(PlainButtonStyle())
+                            .background(Color(hex:"#00FFFF").opacity(0.55))
+                            .cornerRadius(8)
+                            .clipped()
+                        })
+
+                }.padding(.trailing,18)
+                
+                List(self.homeData.ectdClientList.indices,id: \.self) { index in
+                    CardItemView(options:homeData.ectdClientList[index],idx: index)
+                } .onReceive(timer, perform: {_ in
+                    Task {
+                        await homeData.WatchListenEtcdClient()
+                    }
+                }).onDisappear {
+                    self.timer.upstream.connect().cancel()
+                }.onAppear{
+                    Task {
+                        await homeData.WatchListenEtcdClient()
+                    }
+                }.onReceive(closePublisher) { _ in
+                    self.homeData.ectdClientList.removeAll()
                 }
-                .padding(DefaultSpacePadding)
-                .buttonStyle(PlainButtonStyle())
-                .background(Color(hex:"#00FFFF").opacity(0.55))
-                .cornerRadius(8)
-                .clipped()
-                .onTapGesture {
-                    
-                }
-            }.padding(.trailing,18)
-            
-            List(self.homeData.ectdClientList.indices,id: \.self) { index in
-                CardItemView(options:homeData.ectdClientList[index],idx: index)
-            } .onReceive(timer, perform: {_ in
-                Task {
-                    await homeData.WatchListenEtcdClient()
-                }
-            }).onDisappear {
-                self.timer.upstream.connect().cancel()
-            }.onAppear{
-                Task {
-                    await homeData.WatchListenEtcdClient()
-                }
-            }.onReceive(closePublisher) { _ in
-                self.homeData.ectdClientList.removeAll()
+                
             }
-            
-        }
+        }.navigationBarHidden(true)
     }
 }
 
