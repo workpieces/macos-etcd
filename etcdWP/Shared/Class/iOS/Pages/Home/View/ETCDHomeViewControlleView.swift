0ddff13aaf5edf7479cdc56c8eaffa95123fb4e3
Google <zlbfzl@163.com>
2022-07-22 22:17:30 +0800

    [1.0.8][MOD]:修改

--- etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
+++ etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeViewControlleView.swift
@@ -6,58 +6,51 @@
 //
 
 import SwiftUI
+import SwiftUIRouter
 
 struct ETCDHomeViewControlleView: View {
+    var body: some View {
+        SwitchRoutes {
+            Route("create/*") {
+                ETCDConfigView()
+            }
+            Route(content: ETCDHomeContentItemView())
+        }
+    }
+}
+
+
+
+struct ETCDHomeViewiPhoneContentView: View {
     
     @EnvironmentObject var homeData: HomeViewModel
     let closePublisher = NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)
     let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        NavigationView{
-            VStack(spacing: 0) {
-                HStack {
-                    Spacer()
-                    NavigationLink(
-                        destination:  Text(LocalizedStringKey("创建ETCD客户端"))
-                            .withDefaultContentTitle(),
-                        label: {
-                            HStack {
-                                Text(LocalizedStringKey("创建ETCD客户端"))
-                                    .withDefaultContentTitle()
-                            }
-                            .padding(DefaultSpacePadding)
-                            .buttonStyle(PlainButtonStyle())
-                            .background(Color(hex:"#00FFFF").opacity(0.55))
-                            .cornerRadius(8)
-                            .clipped()
-                        })
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+               v
 
-                }.padding(.trailing,18)
-                
-                List(self.homeData.ectdClientList.indices,id: \.self) { index in
-                    CardItemView(options:homeData.ectdClientList[index],idx: index)
-                } .onReceive(timer, perform: {_ in
-                    Task {
-                        await homeData.WatchListenEtcdClient()
-                    }
-                }).onDisappear {
-                    self.timer.upstream.connect().cancel()
-                }.onAppear{
-                    Task {
-                        await homeData.WatchListenEtcdClient()
-                    }
-                }.onReceive(closePublisher) { _ in
-                    self.homeData.ectdClientList.removeAll()
+            }.padding(.trailing,18)
+            
+            List(self.homeData.ectdClientList.indices,id: \.self) { index in
+                CardItemView(options:homeData.ectdClientList[index],idx: index)
+            } .onReceive(timer, perform: {_ in
+                Task {
+                    await homeData.WatchListenEtcdClient()
                 }
-                
+            }).onDisappear {
+                self.timer.upstream.connect().cancel()
+            }.onAppear{
+                Task {
+                    await homeData.WatchListenEtcdClient()
+                }
+            }.onReceive(closePublisher) { _ in
+                self.homeData.ectdClientList.removeAll()
             }
-        }.navigationBarHidden(true)
-    }
-}
-
-struct ETCDHomeViewControlleView_Previews: PreviewProvider {
-    static var previews: some View {
-        ETCDHomeViewControlleView()
+            
+        }
     }
 }
