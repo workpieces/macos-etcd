eebbb72f3a2594cdd8324339f85a1e0908bccf4b
FaceBook <FaceBook@inke.cn>
2022-07-29 22:10:36 +0800

    [MOD]:增加首页

--- etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeController.swift
+++ etcdWP/Shared/Class/iOS/Pages/Home/View/ETCDHomeController.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ETCDHomeController: View {
+    @EnvironmentObject var homeData: HomeViewModel
+    @State private var seletcd = false
+    let closePublisher = NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)
+    let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
+    @State   private  var  choice  = false
+    
     var body: some View {
         GeometryReader { proxy in
             ZStack{
@@ -24,22 +30,58 @@ struct ETCDHomeController: View {
                             .font(.title)
                             .fontWeight(.semibold)
                         Spacer()
-                        Image(systemName: "plus.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .font(.system(size: 18))
-                            .padding(10)
-                            .frame(width: 45, height: 45)
+                        HStack{
+                            Text("全选")
+                                .font(.system(size: 18))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                            ETCDCheckBoxView(IsChoice: $choice, callback:{ newValue in
+                                homeData.selectedItems.removeAll()
+                                if newValue {
+                                    homeData.ectdClientList.forEach { (vaule) in
+                                        var tempValue = vaule
+                                        tempValue.checked = newValue
+                                        homeData.selectedItems.append(tempValue)
+                                    }
+                                }else{
+                                    homeData.ectdClientList.forEach { (vaule) in
+                                        var tempValue = vaule
+                                        tempValue.checked = newValue
+                                    }
+                                }
+                            })
+                        }.padding(.trailing,10)
+                        if homeData.selectedItems.count != 0 {
+                            Button {
+                                homeData.selectedItems.forEach { item in
+                                    let index = self.homeData.ectdClientList.index(of: item)
+                                    if index == nil{
+                                        return
+                                    }
+                                    self.homeData.Delete(id:self.homeData.GetUUID(idx:index!))
+                                }
+                                homeData.selectedItems.removeAll()
+                                self.seletcd = false;
+                            } label: {
+                                HStack {
+                                    Text(LocalizedStringKey("全部删除"))
+                                        .withDefaultContentTitle(fontSize: 10)
+                                }
+                                .padding(8)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            .background(Color(hex:"#00FFFF").opacity(0.55))
+                            .cornerRadius(5)
+                            .clipped()
+                        }
                     }.frame(height:proxy.safeAreaInsets.top)
                     ScrollView(showsIndicators:false){
                         ETCDADBannerTipView()
                             .frame( height: proxy.safeAreaInsets.top)
                             .padding(.bottom,15)
                         LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())],spacing: 5){
-                            ForEach(0 ..< 100){ idx in
-                                RoundedRectangle(cornerRadius: 5)
-                                    .foregroundColor(Color(hue: 0.1 * Double(idx) , saturation: 1, brightness: 1))
-                                .frame(height: 120)
+                            ForEach(Array(self.homeData.ectdClientList.indices),id: \.self) { index in
+                                CardItemView(options:homeData.ectdClientList[index],idx: index)
                             }
                         }
                         ETCDADBannerTipView()
@@ -51,6 +93,18 @@ struct ETCDHomeController: View {
                     .padding(.bottom,10)
                 }
                 
+            }.onReceive(timer, perform: {_ in
+                Task {
+                    await homeData.WatchListenEtcdClient()
+                }
+            }).onDisappear {
+                self.timer.upstream.connect().cancel()
+            }.onAppear{
+                Task {
+                    await homeData.WatchListenEtcdClient()
+                }
+            }.onReceive(closePublisher) { _ in
+                self.homeData.ectdClientList.removeAll()
             }
         }
         
