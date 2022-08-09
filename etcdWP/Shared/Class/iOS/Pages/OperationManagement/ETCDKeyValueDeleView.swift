e59ee665077b91df9e4cad7e7ff7dd47278bbb57
Google <zlbfzl@163.com>
2022-08-09 23:24:48 +0800

    [1.0.8][MOD]:增加前缀

--- etcdWP/Shared/Class/iOS/Pages/OperationManagement/ETCDKeyValueDeleView.swift
+++ etcdWP/Shared/Class/iOS/Pages/OperationManagement/ETCDKeyValueDeleView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 import SwiftUIRouter
+import PopupView
 let KeyValueDeleRouterName = "KeyValueDele"
 struct ETCDKeyValueDeleView: View {
     @EnvironmentObject private var navigator: Navigator
+    @EnvironmentObject var storeObj : ItemStore
+    @State var isShowToast: Bool = false
+    @State var keyPrefixText: String = ""
+    @State var isSucceful :Bool  = false
     init() {
         UITextField.appearance().backgroundColor = .clear
     }
@@ -28,7 +33,7 @@ struct ETCDKeyValueDeleView: View {
                                 navigator.goBack()
                             }
                         Spacer()
-                        Text("study swiftui ")
+                        Text(LocalizedStringKey("键值前缀删除"))
                             .font(.title)
                             .fontWeight(.semibold)
                         Spacer()
@@ -39,7 +44,49 @@ struct ETCDKeyValueDeleView: View {
                             .padding(10)
                             .frame(width: 45, height: 45)
                     }.frame(height:proxy.safeAreaInsets.top)
-                    Text("Text").frame(maxWidth: .infinity, maxHeight: .infinity)
+                    ETCDADBannerTipView()
+                        .frame( height:44)
+                    HStack(){
+                        Text("请输入键值前缀")
+                            .foregroundColor(Color.white)
+                            .font(.custom("HelveticaNeue", size: 14))
+                            .lineSpacing(1.5)
+                            .padding(10)
+                        TextField.init("请输入前缀key", text: $keyPrefixText).textFieldStyle(.roundedBorder)
+                            .padding(.top,10)
+                            .padding(.trailing,10)
+                            .padding(.leading,10)
+                            .padding(.bottom,5)
+                    }
+                    Button {
+                        if  keyPrefixText.isEmpty {
+                            self.isShowToast.toggle()
+                            return
+                        }else{
+                            self.isShowToast = false
+                        }
+                        let result = storeObj.DeletePrefix(key: keyPrefixText)
+                        if result?.status  != 200 {
+                            self.isSucceful.toggle()
+                            self.isShowToast.toggle()
+                            return
+                        }else{
+                            self.isShowToast = false
+                            self.isSucceful =  false
+                        }
+                        navigator.goBack()
+                        
+                    } label: {
+                        Text("确定")
+                            .font(.system(size: 14))
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                            .padding(10)
+                    }
+                    ETCDADBannerTipView()
+                        .frame( height:44)
+                }.popup(isPresented: $isShowToast, type: .toast, position: .top, animation: .spring(), autohideIn: 5) {
+                    TopToastView(title:self.isSucceful ? "保存失败" : "输入错误，请重新输入")
                 }
                 
             }
@@ -48,8 +95,4 @@ struct ETCDKeyValueDeleView: View {
     }
 }
 
-struct ETCDKeyValueDeleView_Previews: PreviewProvider {
-    static var previews: some View {
-        ETCDKeyValueDeleView()
-    }
-}
+
